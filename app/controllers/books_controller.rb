cc76aad5e80e75b19103644c5504339572633249
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-04 03:44:20 +0000

    [complete]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
+before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,10 +35,10 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
@@ -44,11 +47,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-    def correct_user
-    @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(books_path) unless @user == current_user
+    def ensure_correct_user
+     @book = Book.find(params[:id])
+     unless @book.user == current_user
+     redirect_to books_path
+     end
     end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to users_path(@user.id), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render :edit
     end
