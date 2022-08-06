7008268fd5a29eb8ae8d64a32607ce5ac698107b
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-06 10:25:07 +0000

    [finish]ouyou3

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,10 @@ class User < ApplicationRecord
   has_many :books,  dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :favorites, dependent: :destroy
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
 end
