55f178718e26224e152656dd4256c2d56324db07
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-08 05:21:12 +0000

    [complete]ouyou4

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,7 +3,4 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
-  end
 end
