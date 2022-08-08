fce497bc15abef2b6a753573adb6f7256274d206
ohno0722 <108727779+ohno0722@users.noreply.github.com>
2022-08-08 09:27:03 +0000

    [finish]ouyou5

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"homes#about", as: :home_about
 
+  get "search" => "searches#search"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
